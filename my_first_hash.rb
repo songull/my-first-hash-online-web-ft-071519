d047afb38832868e40f2a2454c2c4783b597fbba
Kyler Barber <kylerbarber820@yahoo.com>
2019-07-28 19:08:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,6 +19,6 @@ end
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3, "muskets" => 2, "gun powder" => 4}
 
-  shipping_manifest["pearl necklace"] = 1
+  shipping_manifest["pearl necklace"] => 1
   shipping_manifest
 end
